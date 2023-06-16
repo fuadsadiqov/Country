09392b3072da1c377cb06d1622e6d1982d101b88
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-06-16 15:23:22 +0400

    adding paging

--- src/app/country/country-list/country-list.component.ts
+++ src/app/country/country-list/country-list.component.ts
@@ -13,10 +13,11 @@ export class CountryListComponent {
   private isSortedName: boolean = false
   private isSortedPopulation: boolean = false
   private isSortedArea: boolean = false
+  public pagingNumber: Array<number> = []
 
   constructor(private restService: RestService){
     this.restService.getCountry()
-    .subscribe((res: any) => {            
+    .subscribe((res: any) => {                  
       this.countryList = res.pageProps.countries.map((item: any) => ({        
         name: item.name.common,
         population: item.population,
@@ -26,33 +27,46 @@ export class CountryListComponent {
         query: item['cca3'],
         region: item.region
       }))
+      for (let index = 0; index < this.countryList.length / 10; index++) {
+        this.pagingNumber[index] = index + 1;
+      }
+      this.filteredCountryList = this.countryList.slice(0, 10)
     })
+  }
+  activePage(index: number){
+    let startPaging = index * 10
+    let endPaging = (index * 10) + 10
+    this.filteredCountryList = this.countryList.slice(startPaging, endPaging)
   }
   sortByName(){
     this.isSortedName = !this.isSortedName
     if(this.isSortedName){
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => a.name.localeCompare(b.name))
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => a.name.localeCompare(b.name))
     }else{
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => b.name.localeCompare(a.name))
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => b.name.localeCompare(a.name))
     }
   }
   sortByArea(){
     this.isSortedArea = !this.isSortedArea 
     if(this.isSortedArea){
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => a.area - b.area)
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => a.area - b.area)
     }else{
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => b.area - a.area)
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => b.area - a.area)
     }
   }
   sortByPopulation(){
     this.isSortedPopulation = !this.isSortedPopulation
     if(this.isSortedPopulation){
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => a.population - b.population)
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => a.population - b.population)
     }else{
-      this.filteredCountryList = this.countryList.sort((a: any, b: any) => b.population - a.population)
+      this.filteredCountryList = this.filteredCountryList.sort((a: any, b: any) => b.population - a.population)
     }
   }
   searchFunction(value: string){
-    this.filteredCountryList = this.countryList.filter((item: CountryListInterface) => item.name.toLowerCase().includes(value.toLowerCase()))
+    if(value != ''){
+      this.filteredCountryList = this.countryList.filter((item: CountryListInterface) => item.name.toLowerCase().includes(value.toLowerCase()))
+    }else{
+      this.filteredCountryList = this.countryList.slice(0, 10)
+    }
   }
 }
